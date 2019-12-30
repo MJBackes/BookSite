9bdafe49955a0c61fe92be4dc3e6302f627cb9eb
MJBackes <mjohnbackes@gmail.com>
2019-12-30 11:08:08 -0600

    Added an EmailService Class to send emails to users.

---
+++ BookSite/Email/EmailService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using BookSite.Models.EmailModels;
+using MailKit;
+using MailKit.Net.Smtp;
+using MimeKit;
+namespace BookSite.Email
+{
+    public class EmailService
+    {
+
+        public static async Task SendEmail(EmailModel model)
+        {
+            var email = new MimeMessage();
+            email.From.Add(new MailboxAddress(model.FromDisplayName, model.FromEmailAddress));
+            email.To.Add(new MailboxAddress(model.ToName, model.ToEmailAddress));
+            email.Subject = model.Subject;
+            var body = new BodyBuilder { TextBody = model.Message };
+            email.Body = body.ToMessageBody();
+            using (var emailClient = new SmtpClient())
+            {
+                emailClient.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                await emailClient.ConnectAsync("smtp.gmail.com", 465, true).ConfigureAwait(false);
+                await emailClient.AuthenticateAsync(EmailInfo.EmailInfo.Address, EmailInfo.EmailInfo.Pass).ConfigureAwait(false);
+                await emailClient.SendAsync(email).ConfigureAwait(false);
+                await emailClient.DisconnectAsync(true).ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ BookSite/Models/EmailModels/EmailModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookSite.Models.EmailModels
+{
+    public class EmailModel
+    {
+        public string FromDisplayName { get; set; }
+        public string FromEmailAddress { get; set; }
+        public string ToName { get; set; }
+        public string ToEmailAddress { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
